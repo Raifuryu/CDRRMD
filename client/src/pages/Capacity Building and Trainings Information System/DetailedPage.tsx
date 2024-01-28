7b6bb56167659f873afb823f605e209413f4309d
Raifuryu <jhudielabrogar@gmail.com>
2024-01-28 23:32:00 +0800

    CBTIS Update, Mass Participants Upload

--- client/src/pages/Capacity Building and Trainings Information System/DetailedPage.tsx
+++ client/src/pages/Capacity Building and Trainings Information System/DetailedPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Papa from "papaparse";
 import {
   Button,
   Col,
@@ -16,22 +17,64 @@ import {
   Spin,
   Table,
   Typography,
+  Upload,
+  UploadProps,
+  message,
 } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined, UploadOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 
 const { RangePicker } = DatePicker;
+const { Option } = Select;
+const { Title } = Typography;
 
 const DeatailedPage = () => {
-  interface barangay {
+  interface Barangay {
     id: number;
     fk_city_id: number;
     name: string;
   }
 
+  interface Participant {
+    first_name: string;
+    middle_name: string;
+    last_name: string;
+    extension_name: string;
+    barangay: string;
+    blood_type: string;
+  }
+
+  const ParticipantColumn = [
+    {
+      title: "First Name",
+      dataIndex: "first_name",
+      key: "first_name",
+    },
+    {
+      title: "Middle Name",
+      dataIndex: "middle_name",
+      key: "middle_name",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+    {
+      title: "Barangay",
+      dataIndex: "barangay",
+      key: "barangay",
+    },
+    {
+      title: "Blood Type",
+      dataIndex: "blood_type",
+      key: "blood_type",
+    },
+  ];
+
   const { trainingId } = useParams();
 
-  const [barangay, setBarangay] = useState<barangay[]>([]);
+  const [barangay, setBarangay] = useState<Barangay[]>([]);
 
   const [trainingTitle, setTrainingTitle] = useState("");
   const [trainingVenue, setTrainingVenue] = useState("");
@@ -39,8 +82,10 @@ const DeatailedPage = () => {
   const [trainingStartDate, setTrainingStartDate] = useState();
   const [trainingEndDate, setTrainingEndDate] = useState();
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [drawerState, setDrawerState] = useState(false);
+
+  const [participants, setParticipants] = useState<Participant[]>([]);
 
   const showDrawer = () => {
     setDrawerState(true);
@@ -51,24 +96,63 @@ const DeatailedPage = () => {
   };
 
   useEffect(() => {
-    fetch(`http://192.168.1.69:3000/training/${trainingId}`)
-      .then((res) => res.json())
-      .then((result) => {
-        const firstItem = result[0];
+    const trainingDetailsRequest = fetch(
+      `http://192.168.1.69:3000/training/${trainingId}`
+    ).then((res) => res.json());
 
-        setTrainingTitle(firstItem.title);
-        setTrainingVenue(firstItem.venue);
-        setTrainingTrainer(firstItem.trainer);
-        setTrainingStartDate(firstItem.start_date);
-        setTrainingEndDate(firstItem.end_date);
-        // fetch(`http://192.168.1.69:3000/barangay`)
-        //   .then((res) => res.json())
-        //   .then((result) => {
-        //     setBarangay(result);
-        //   });
+    const barangayRequest = fetch("http://192.168.1.69:3000/barangay").then(
+      (res) => res.json()
+    );
+
+    Promise.all([trainingDetailsRequest, barangayRequest])
+      .then(([trainingData, barangayData]) => {
+        setTrainingTitle(trainingData.title);
+        setTrainingVenue(trainingData.venue);
+        setTrainingTrainer(trainingData.trainer);
+        setTrainingStartDate(trainingData.start_date);
+        setTrainingEndDate(trainingData.end_date);
+
+        setBarangay(barangayData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        // Handle error appropriately
       })
       .finally(() => setLoading(false));
   }, []);
+
+  const handleFileUpload = (file: File) => {
+    Papa.parse<Participant>(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: (result) => {
+        setParticipants(result.data as Participant[]);
+        console.log(participants);
+        message.success(`${file.name} file uploaded successfully`);
+      },
+      error: (error) => {
+        console.error("CSV parsing error:", error.message);
+        message.error(`${file.name} file upload failed.`);
+      },
+    });
+  };
+
+  const props: UploadProps = {
+    name: "file",
+    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+    headers: {
+      authorization: "authorization-text",
+    },
+    beforeUpload: (file: File) => {
+      const isCSV = file.type === "application/vnd.ms-excel";
+      if (!isCSV) {
+        message.error(`${file.name} is not a CSV file`);
+      } else {
+        handleFileUpload(file);
+      }
+      return isCSV;
+    },
+  };
 
   if (loading) {
     return <Spin />;
@@ -136,14 +220,6 @@ const DeatailedPage = () => {
       </Flex>
       <Flex gap={16}>
         <Flex vertical gap={16}>
-          {/* <Button
-            shape="round"
-            size="large"
-            onClick={showDrawer}
-            icon={<PlusOutlined />}
-          >
-            Add Participants
-          </Button> */}
           <FloatButton
             shape="circle"
             type="primary"
@@ -165,105 +241,102 @@ const DeatailedPage = () => {
           </Space>
         }
       >
-        <Form layout="vertical" hideRequiredMark>
+        <Form layout="vertical">
+          <Title level={3}>Add Participants</Title>
+          <Divider orientation="left">
+            <Title level={3}>Participant Details</Title>
+          </Divider>
           <Row gutter={16}>
-            <Col span={12}>
+            <Col span={6}>
               <Form.Item
-                name="name"
-                label="Name"
+                name="first_name"
+                label="First Name"
                 rules={[{ required: true, message: "Please enter user name" }]}
               >
-                <Input placeholder="Please enter user name" />
+                <Input placeholder="John" />
               </Form.Item>
             </Col>
-            <Col span={12}>
+            <Col span={6}>
+              <Form.Item name="middle_name" label="Middle Name">
+                <Input style={{ width: "100%" }} placeholder="The" />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
               <Form.Item
-                name="url"
-                label="Url"
-                rules={[{ required: true, message: "Please enter url" }]}
+                name="last_name"
+                label="Last Name"
+                rules={[{ required: true, message: "Doe" }]}
               >
                 <Input
                   style={{ width: "100%" }}
-                  addonBefore="http://"
-                  addonAfter=".com"
                   placeholder="Please enter url"
                 />
               </Form.Item>
             </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                name="owner"
-                label="Owner"
-                rules={[{ required: true, message: "Please select an owner" }]}
-              >
-                <Select placeholder="Please select an owner">
-                  <Option value="xiao">Xiaoxiao Fu</Option>
-                  <Option value="mao">Maomao Zhou</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="type"
-                label="Type"
-                rules={[{ required: true, message: "Please choose the type" }]}
-              >
-                <Select placeholder="Please choose the type">
-                  <Option value="private">Private</Option>
-                  <Option value="public">Public</Option>
-                </Select>
+            <Col span={6}>
+              <Form.Item name="extension_name" label="Extension Name">
+                <Input style={{ width: "100%" }} placeholder="Sr. Jr. III..." />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={16}>
             <Col span={12}>
               <Form.Item
-                name="approver"
-                label="Approver"
+                name="brangay"
+                label="Barangay"
                 rules={[
-                  { required: true, message: "Please choose the approver" },
+                  { required: true, message: "Please select a Barangay" },
                 ]}
               >
-                <Select placeholder="Please choose the approver">
-                  <Option value="jack">Jack Ma</Option>
-                  <Option value="tom">Tom Liu</Option>
+                <Select placeholder="Please select a Barangay">
+                  {barangay.map((item, index) => (
+                    <Option key={index}>{item.name}</Option>
+                  ))}
                 </Select>
               </Form.Item>
             </Col>
             <Col span={12}>
               <Form.Item
-                name="dateTime"
-                label="DateTime"
+                name="blood_type"
+                label="Blood Type"
                 rules={[
-                  { required: true, message: "Please choose the dateTime" },
+                  { required: true, message: "Please choose Blood Type" },
                 ]}
               >
-                <DatePicker.RangePicker
-                  style={{ width: "100%" }}
-                  getPopupContainer={(trigger) => trigger.parentElement!}
-                />
+                <Select placeholder="Select Blood Type">
+                  <Option value="A+">A+</Option>
+                  <Option value="A">A</Option>
+                  <Option value="A-">A-</Option>
+                  <Option value="AB+">AB+</Option>
+                  <Option value="AB">AB</Option>
+                  <Option value="AB-">AB-</Option>
+                  <Option value="B+">B+</Option>
+                  <Option value="B">B</Option>
+                  <Option value="B-">B-</Option>
+                  <Option value="O+">O+</Option>
+                  <Option value="O">O</Option>
+                  <Option value="O-">O-</Option>
+                </Select>
               </Form.Item>
             </Col>
           </Row>
-          <Row gutter={16}>
+          <Row>
+            <Divider orientation="left">
+              <Title level={3}>Mass Upload Participants</Title>
+            </Divider>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <Upload {...props} accept=".csv" maxCount={1}>
+                <Button icon={<UploadOutlined />}>
+                  Upload CSV participants
+                </Button>
+              </Upload>
+            </Col>
+          </Row>
+          <Row>
             <Col span={24}>
-              <Form.Item
-                name="description"
-                label="Description"
-                rules={[
-                  {
-                    required: true,
-                    message: "please enter url description",
-                  },
-                ]}
-              >
-                <Input.TextArea
-                  rows={4}
-                  placeholder="please enter url description"
-                />
-              </Form.Item>
+              <Table dataSource={participants} columns={ParticipantColumn} />
             </Col>
           </Row>
         </Form>
